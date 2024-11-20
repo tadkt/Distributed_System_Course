4b528ba28233ae2ac793b4bc218ce38c62abc9c4
Ha Nguyen <hanh.ba12-066@st.usth.edu.vn>
2024-11-20 23:01:38 +0700

    Save changes to client.py before merge

--- client.py
+++ client.py
@@ -1,15 +1,18 @@
 import socket
+def client():
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#ivp4,for streaming
+    client.connect((socket.gethostname(),1234)) 
+    file = input("Filename: ")
+    client.sendall(file.encode())
+    with open(f"downloaded_{file}","wb") as f:
+        while True: 
+            data = client.recv(1024)
+            if data == b'EOF':  # Check for end-of-file marker
+                break
+            f.write(data)
+    print("Original file: ",file) 
+    print("File on my pc: ",f"downloaded_{file}")
 
-#Setup server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+if __name__ == "__main__":
+    client()
 
-full_msg = b''
-while True:
-    chunk = s.recv(1024) #Receive data from chunk
-    if not chunk:
-        break
-    full_msg += chunk
-
-print(full_msg.decode("utf-8"))
-s.close()
