bec46ae2317fce8244fa8c1d15f3e78760ac3728
Ha Nguyen <hanh.ba12-066@st.usth.edu.vn>
2024-11-20 15:39:41 +0700

    Ha update main()

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 import socket
 
+#Setup server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-full_msg = ''
+full_msg = b''
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
+    chunk = s.recv(1024) #Receive data from chunk
+    if not chunk:
         break
-    full_msg += msg.decode("utf-8")
+    full_msg += chunk
 
-print(full_msg)
+print(full_msg.decode("utf-8"))
+s.close()
--- server.py
+++ server.py
@@ -1,11 +1,29 @@
 import socket
 
+def sendfile(client_socket, filename):
+    try:
+        with open(filename, 'rb') as file:
+            chunk = file.read(1024)  
+            while chunk:
+                client_socket.send(chunk)
+                chunk = file.read(1024)
+    except FileNotFoundError:
+        error_message = f"Error: File '{filename}' not found."
+        print(error_message)  
+        client_socket.send(bytes(error_message, "utf-8"))  
+
+#Server setup
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
 
+print("Server is listening...")
+
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+
+    #Send file
+    sendfile(clientsocket, "example.txt")
+
     clientsocket.close()
