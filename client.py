e4849076096dbf6317924ce74431b78b98eca19b
Lemonator27 <buidanhlam27@gmail.com>
2024-11-20 14:04:48 +0700

    added feature to receive specific file from the server

--- client.py
+++ client.py
@@ -1,5 +1,17 @@
 import socket
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#ivp4,for streaming
-s.connect((socket.gethostname(),1234)) 
-msg = s.recv(1024) #buffer memory
-print(msg.decode("utf-8"))
+def client():
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#ivp4,for streaming
+    client.connect((socket.gethostname(),1234)) 
+    file = input("Filename: ")
+    client.sendall(file.encode())
+    with open(f"downloaded_{file}","wb") as f:
+        while True: 
+            data = client.recv(1024)
+            if not data:
+                break
+            f.write()
+    print("Original file: ",file) 
+    print("File on my pc: ",f"downloaded_{file}")
+
+if __name__ == "__main__":
+    client()
