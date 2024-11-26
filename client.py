8bfa963912ad76b8a80af1547bc3bc26bb7c042b
akashi0310 <daohoangdung2k4@gmail.com>
2024-11-26 14:19:08 +0700

    contact among clients full

--- client.py
+++ client.py
@@ -3,14 +3,14 @@ import select
 import errno
 import sys
 
-header_length = 10
+header_length = 10 #the length of message header
 ip = '127.0.0.1'
 port =1235
 
-my_username = input('Username: ')
+my_username = input('Enter your user name: ')
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((ip,port))
-client_socket.setblocking(False)
+client_socket.connect((ip,port)) 
+client_socket.setblocking(False) #this allow the program to run while waiting for data
 
 username = my_username.encode('utf-8')
 username_header = f'{len(username):<{header_length}}'.encode('utf-8')
@@ -27,9 +27,10 @@ while True:
         while True:
             username_header = client_socket.recv(header_length)
             if not len(username_header):
-                print('connection closed by server')
+                print('Connection closed by server')
                 sys.exit()
-            username_length = int(username_length.decode('utf-8').strip())
+
+            username_length = int(username_header.decode('utf-8').strip())
             username = client_socket.recv(username_length).decode('utf-8')
             
             message_header = client_socket.recv(header_length)
